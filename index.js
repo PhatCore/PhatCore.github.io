726fc64905e97a5bcade4d16fbc1a130fbf83d7a
PhatCore <cophatartstruct@gmail.com>
2022-12-18 18:09:46 +0700

    upload website first time-Ver1

---
+++ index.js
@@ -0,0 +1,29 @@
+﻿ $(function(){
+    positionblockEdu = $('#blockEdu').offset().top;
+    positionbblockPortfolio = $('#blockPortfolio').offset().top;
+    positionblockContact = $('#blockContact').offset().top;
+
+    $(".effectBtn,.exp").click(function () { 
+        $('body,html').animate({scrollTop:positionblockEdu-80},600)
+        return false;
+    })  
+    $(".port").click(function () { 
+        $('body,html').animate({scrollTop:positionbblockPortfolio-180},600)
+        return false;
+    })  
+    $(".linkCon").click(function () { 
+      $('body,html').animate({scrollTop:positionblockContact+200},600)
+      return false;
+  }) 
+}) 
+
+var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    columnWidth: '.grid-sizer'
+  });
+  // layout Masonry after each image loads
+  $grid.imagesLoaded().progress( function() {
+    $grid.masonry();
+  });  
+ 
